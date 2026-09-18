2b4e65be00a47721cfd0426d17b1ffcfb7f9967b
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2026-09-18 22:05:46 +0200

    feat(core): export board movement options

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,4 +1,5 @@
 export { Board } from "./Board.js";
+export type { BoardOptions } from "./Board.js";
 export { BoardStateError } from "./errors.js";
 export type { BoardStateErrorCode } from "./errors.js";
 export type { PieceId, SpaceId } from "./ids.js";
