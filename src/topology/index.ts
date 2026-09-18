6fa0425a6d3fdd58df7e0b4e2eca04e8fce99db6
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2026-09-18 21:58:54 +0200

    feat(topology): add graph topology for #2

---
+++ src/topology/index.ts
@@ -0,0 +1,5 @@
+export { GraphTopology } from "./GraphTopology.js";
+export type { GraphConnectionOptions } from "./GraphTopology.js";
+export { TopologyError } from "./errors.js";
+export type { TopologyErrorCode } from "./errors.js";
+export type { Topology } from "./Topology.js";
