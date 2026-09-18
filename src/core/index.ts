0a4da3b8860f9ce858c4b4cfca81736a91645b72
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2026-09-18 22:08:26 +0200

    feat(core): export occupancy policy API

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,5 +1,6 @@
 export { Board } from "./Board.js";
 export type { BoardOptions } from "./Board.js";
+export type { OccupancyPolicy, OccupancyPolicyContext } from "./occupancy.js";
 export { BoardStateError } from "./errors.js";
 export type { BoardStateErrorCode } from "./errors.js";
 export type { PieceId, SpaceId } from "./ids.js";
