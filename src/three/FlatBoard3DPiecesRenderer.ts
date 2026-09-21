b533bfe0eac895f74cfb5b9585ef65e4a134331b
PawelWielga <132073666+PawelWielga@users.noreply.github.com>
2026-09-21 07:39:04 +0200

    fix: normalize renderer label imports

--- src/three/FlatBoard3DPiecesRenderer.ts
+++ src/three/FlatBoard3DPiecesRenderer.ts
@@ -44,7 +44,11 @@ import {
   applyThreeAppearanceTransform,
   resolveThreeAppearanceTransform,
 } from "./appearanceMapping.js";
-import { resolveThreeAppearanceAssetKind } from "./assets.js";\nimport {\n  createPieceLabelSprite,\n  disposeRendererOwnedLabelTexture,\n} from "./pieceLabel.js";
+import { resolveThreeAppearanceAssetKind } from "./assets.js";
+import {
+  createPieceLabelSprite,
+  disposeRendererOwnedLabelTexture,
+} from "./pieceLabel.js";
 import type {
   ThreeBoardAsset,
   ThreeBoardAssetProvider,
